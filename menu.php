2acf6613ad67d02801cd6a0e26ea640be49ac3c3
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-04-14 19:01:27 +0300

    Menu started

---
+++ menu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset = "utf-8">
+        <title> SNOWBOARDSHOP.COM</title> 
+        <link rel = "stylesheet" href = "CSS/mystyle.css">   
+    </head>
+    
+    <body>
+        <h1 id="header">
+        Greenhouse
+        </h1>
+
+<div id="content">
