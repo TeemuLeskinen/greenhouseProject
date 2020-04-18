eabfedf5a9cb07efd918cadf8ea00def453fc61e
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-04-18 08:39:33 +0300

    database connection

--- serialConnection.py
+++ serialConnection.py
@@ -5,18 +5,24 @@ import time
 import mysql.connector
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
+cnx = mysql.connector.connect(user='testaaja', password='projekti',
+                              host='localhost', database='testdatabase')
 
-while 1:
-    try:
-        ser_bytes = ser.readline()
-        decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-        print(decoded_bytes)
-        temp = decoded_bytes
-        con = mysql.connector.connect('localhost','testaaja','projekti','mittari')
-        with con:
-            cursor = con.cursor()
-            cursor.execute("INSERT INTO mittari VALUES (,(%f),'',''" %temp);
-            con.commit()
-            cursor.close()
-    except:
-        print("error")
+ser_bytes = ser.readline()
+decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+temp = decoded_bytes
+temperature = ser.readline()
+
+cursor = cnx.cursor()
+
+add_temperature = ("INSERT INTO mittari"
+                   "(temperature)"
+                   "VALUES (%s)")
+data_temperature = (float(temperature))
+
+cursor.execute(add_temperature, data_temperature)
+cnx.commit()
+
+cursor.close()
+cnx.close()
+
