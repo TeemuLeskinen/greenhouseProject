35e59a3b9f839be40606ce1b5d3e8b160902ebda
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-04-19 19:51:55 +0300

    Serial connection works

--- serialConnection.py
+++ serialConnection.py
@@ -1,26 +1,27 @@
-#!/usr/bin/env python
-
+import mysql.connector
 import serial
 import time
-import mysql.connector
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
+
 cnx = mysql.connector.connect(user='testaaja', password='projekti',
-                              host='localhost', database='testdatabase')
+                              host='localhost',
+                              database='testdatabase')
+
 
 ser_bytes = ser.readline()
 decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-temp = decoded_bytes
-temperature = ser.readline()
 
 cursor = cnx.cursor()
 
-add_temperature = ("INSERT INTO mittari"
-                   "(temperature)"
-                   "VALUES (%s)")
-data_temperature = (float(temperature))
+lisaa_temp = ("INSERT INTO mittari "
+               "(user, temperature) "
+               "VALUES (%s, %s)")
 
-cursor.execute(add_temperature, data_temperature)
+data_temp = ('TL', float(decoded_bytes))
+
+cursor.execute(lisaa_temp, data_temp)
+
 cnx.commit()
 
 cursor.close()
