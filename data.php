9bed1dce60d769b5c7832357587ad77e12e6d090
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-04-20 19:49:48 +0300

    Update

--- data.php
+++ data.php
@@ -1,36 +1,17 @@
 <?php include "menu.php"; ?>
 <?php include "connection.php"; ?>
-<<<<<<< HEAD
+
 <h2> Temp </h2>
+
 <?php
-$sql="SELECT temperature FROM mittari ";
-=======
-<?php
-<<<<<<< HEAD
-$sql="SELECT temperature FROM mittari";
->>>>>>> data.php
-=======
 $sql="SELECT * FROM mittari";
->>>>>>> data.php
 $temperature = $db->query($sql); 
 ?>
 
 	<h2> Temperature data </h2>
 	<p>
 		<ul>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-			<?php
-
-			
-			
-			$temp = array("22", "25", "32");
->>>>>>> data.php
-			
-=======
-			
-			
+		
 			<?php			
 
 			echo '<h3>Temperature</h3>';
@@ -50,7 +31,7 @@ $temperature = $db->query($sql);
 
 
 			?>
->>>>>>> data.php
+
 		</ul>
 	</p>
 <? include "footer.php"; ?>
