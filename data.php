4936e9b339e3343f25aa4b8c5725663eb0537fed
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-04-20 18:28:04 +0300

    test

--- data.php
+++ data.php
@@ -1,15 +1,16 @@
 <?php include "menu.php"; ?>
+<?php include "connection.php"; ?>
+<?php
+$sql="SELECT temperature FROM mittari";
+$temperature = $db->query($sql); 
+?>
 
 	<h2> Temperature data </h2>
 	<p>
 		<ul>
 			<?php
 
-			/*$temperature = array(
-							array("temperature"=>'22.4')
-							array("humidity"=>'30')
-							);
-			echo 'Temperature is ' .$temperature[0];*/
+			
 			
 			$temp = array("22", "25", "32");
 			
