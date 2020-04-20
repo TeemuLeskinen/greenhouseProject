99438bc41a8f9871c06bb834d9cfdf04526b59d7
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-04-20 19:40:38 +0300

    Data is being retrieved from the database

--- data.php
+++ data.php
@@ -1,20 +1,31 @@
 <?php include "menu.php"; ?>
 <?php include "connection.php"; ?>
 <?php
-$sql="SELECT temperature FROM mittari";
+$sql="SELECT * FROM mittari";
 $temperature = $db->query($sql); 
 ?>
 
 	<h2> Temperature data </h2>
 	<p>
 		<ul>
-			<?php
+			
+			
+			<?php			
 
+			echo '<h3>Temperature</h3>';
+			foreach ($temperature as $row) {				
+				echo '<p>';
+				echo $row['temperature'].'';
+				echo ' c                     ';
+				echo $row['date'].'<br>';
+				echo '</p>';
+				
+				}
+				
+				
+			/*$temp = array("22", "25", "32");
 			
-			
-			$temp = array("22", "25", "32");
-			
-			echo 'Temperature is ' .$temp[1];
+			echo 'Temperature is ' .$temp[1];*/
 
 
 			?>
