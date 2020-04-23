d20c361eb5fe7fd62552dc5fa4d36812b68f7713
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-04-23 18:35:23 +0300

    Some fixes

--- serialConnection.py
+++ serialConnection.py
@@ -8,32 +8,34 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 cnx = mysql.connector.connect(user='testaaja', password='projekti',
                               host='localhost',
                               database='testdatabase')
+try:
+    raw = ser.readline()
+    sleep(0.05)
 
-raw = ser.readline()
-sleep(0.05)
+    decodeSerial = raw.decode('utf-8').strip('\r\n')
 
-decodeSerial = raw.decode('utf-8').strip('\r\n')
+    splitData = decodeSerial.split(',')
 
-splitData = decodeSerial.split(',')
+    temperature = splitData[0]
+    luminousity = splitData[1]
 
-temperature = splitData[0]
-luminousity = splitData[1]
+    temp = float(temperature)
+    lux = int(luminousity)
 
-temp = float(temperature)
-lux = int(luminousity)
+    cursor = cnx.cursor()
 
-cursor = cnx.cursor()
+    lisaa_temp = ("INSERT INTO mittari "
+                  "(user, temperature, luminousity) "
+                  "VALUES (%s, %s, %s)")
 
-lisaa_temp = ("INSERT INTO mittari "
-               "(user, temperature, luminousity) "
-               "VALUES (%s, %s, %s)")
+    data_temp = ('TL', temp, lux)
 
-data_temp = ('TL', temp, lux)
+    cursor.execute(lisaa_temp, data_temp)
 
-cursor.execute(lisaa_temp, data_temp)
+    cnx.commit()
 
-cnx.commit()
+    cursor.close()
+    cnx.close()
 
-cursor.close()
-cnx.close()
-
+except:
+    print("Error occured")
