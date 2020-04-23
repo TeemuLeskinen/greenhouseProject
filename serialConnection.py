6c8d903b0aa4441b22268030c5bebeee28fc6f5f
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-04-23 15:36:13 +0300

    Serial data with temperature and luminousity

--- serialConnection.py
+++ serialConnection.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import serial
 import time
+from time import sleep
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
@@ -8,17 +9,26 @@ cnx = mysql.connector.connect(user='testaaja', password='projekti',
                               host='localhost',
                               database='testdatabase')
 
+raw = ser.readline()
+sleep(0.05)
 
-ser_bytes = ser.readline()
-decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+decodeSerial = raw.decode('utf-8').strip('\r\n')
+
+splitData = decodeSerial.split(',')
+
+temperature = splitData[0]
+luminousity = splitData[1]
+
+temp = float(temperature)
+lux = int(luminousity)
 
 cursor = cnx.cursor()
 
 lisaa_temp = ("INSERT INTO mittari "
-               "(user, temperature) "
-               "VALUES (%s, %s)")
+               "(user, temperature, luminousity) "
+               "VALUES (%s, %s, %s)")
 
-data_temp = ('TL', float(decoded_bytes))
+data_temp = ('TL', temp, lux)
 
 cursor.execute(lisaa_temp, data_temp)
 
